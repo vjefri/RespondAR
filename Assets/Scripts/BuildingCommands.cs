17166a0e63a06763578b83aafc527c2e066a04a2
Jefri Vanegas <vjefri@gmail.com>
2016-06-25 04:54:17 -0700

    voice commands  in progress

---
+++ Assets/Scripts/BuildingCommands.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine;
+
+public class BuildingCommands : MonoBehaviour
+{
+    Vector3 originalPosition;
+    public GameObject Build;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Grab the original local position of the building when the app starts.
+        originalPosition = this.transform.localPosition;
+    }
+
+    // Called by GazeGestureManager when the user performs a Select gesture
+    void OnSelect()
+    {
+        // If the sphere has no Rigidbody component, add one to enable physics.
+        if (!this.GetComponent<Rigidbody>())
+        {
+            var rigidbody = this.gameObject.AddComponent<Rigidbody>();
+            rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        }
+    }
+
+    // Called by SpeechManager when the user says the "Get Building" command
+    void OnAddress()
+    {
+        // spawn the building into the scene
+        Instantiate(Build);
+
+    }
+}
+
