42219e2eac033bd2107b25f5babc757abbc9f55d
Jefri Vanegas <vjefri@gmail.com>
2016-06-25 16:09:17 -0700

    more scripts still working

---
+++ Assets/Scripts/HouseController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HouseController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    // Called by SpeechManager
+    public void OnBigger()
+    {
+        this.gameObject.transform.localScale = new Vector3(this.gameObject.transform.localScale.x * 2, this.gameObject.transform.localScale.y * 2, this.gameObject.transform.localScale.z * 2);
+    }
+
+    // Called by SpeechManager 
+    public void OnSmaller()
+    {
+        this.gameObject.transform.localScale = new Vector3(this.gameObject.transform.localScale.x / 2, this.gameObject.transform.localScale.y / 2, this.gameObject.transform.localScale.z / 2);
+    }
+
+
+}
