0dd00cadd776f779fb8937dc75e45c321ebebe0d
Jefri Vanegas <vjefri@gmail.com>
2016-06-25 21:15:05 -0700

    imported NYC

---
+++ Assets/Scripts/HighlightObject.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickableThing : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            OnSelect();
+        }
+
+    }
+
+    public void OnSelect()
+    {
+        this.gameObject.transform.position += Vector3.up * 0.1f;
+    }
+}
